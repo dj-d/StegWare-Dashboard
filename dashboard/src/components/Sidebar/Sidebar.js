37d5c10c0304b663b9f004cb94a1badc6fcd2884
Daniele Albanese <daniele.albanese98@gmail.com>
2021-03-04 00:32:47 +0100

    Component Sidebar -> reformat code + streamlining interface + Updated name and icons of pages

--- dashboard/src/components/Sidebar/Sidebar.js
+++ dashboard/src/components/Sidebar/Sidebar.js
@@ -1,18 +1,15 @@
-import React, { useState, useEffect } from "react";
-import { Drawer, IconButton, List } from "@material-ui/core";
+import React, {useState, useEffect} from "react";
+import {Drawer, IconButton, List} from "@material-ui/core";
 import {
-  Home as HomeIcon,
-  NotificationsNone as NotificationsIcon,
-  FormatSize as TypographyIcon,
-  FilterNone as UIElementsIcon,
-  BorderAll as TableIcon,
-  QuestionAnswer as SupportIcon,
-  LibraryBooks as LibraryIcon,
-  HelpOutline as FAQIcon,
-  ArrowBack as ArrowBackIcon,
+	Home as HomeIcon,
+	ArrowBack as ArrowBackIcon,
+	Apps as PayloadsIcon,
+	Flag as AttacksIcon,
+	PhoneAndroid as InfectedDevicesIcon,
+	SupervisorAccount as TeamIcon
 } from "@material-ui/icons";
-import { useTheme } from "@material-ui/styles";
-import { withRouter } from "react-router-dom";
+import {useTheme} from "@material-ui/styles";
+import {withRouter} from "react-router-dom";
 import classNames from "classnames";
 
 // styles
@@ -20,137 +17,107 @@ import useStyles from "./styles";
 
 // components
 import SidebarLink from "./components/SidebarLink/SidebarLink";
-import Dot from "./components/Dot";
 
 // context
-import {
-  useLayoutState,
-  useLayoutDispatch,
-  toggleSidebar,
-} from "../../context/LayoutContext";
+import {useLayoutState, useLayoutDispatch, toggleSidebar,} from "../../context/LayoutContext";
 
 const structure = [
-  { id: 0, label: "Dashboard", link: "/app/dashboard", icon: <HomeIcon /> },
-  {
-    id: 1,
-    label: "Typography",
-    link: "/app/typography",
-    icon: <TypographyIcon />,
-  },
-  { id: 2, label: "Tables", link: "/app/tables", icon: <TableIcon /> },
-  {
-    id: 3,
-    label: "Notifications",
-    link: "/app/notifications",
-    icon: <NotificationsIcon />,
-  },
-  {
-    id: 4,
-    label: "UI Elements",
-    link: "/app/ui",
-    icon: <UIElementsIcon />,
-    children: [
-      { label: "Icons", link: "/app/ui/icons" },
-      { label: "Charts", link: "/app/ui/charts" },
-      { label: "Maps", link: "/app/ui/maps" },
-    ],
-  },
-  { id: 5, type: "divider" },
-  { id: 6, type: "title", label: "HELP" },
-  { id: 7, label: "Library", link: "", icon: <LibraryIcon /> },
-  { id: 8, label: "Support", link: "", icon: <SupportIcon /> },
-  { id: 9, label: "FAQ", link: "", icon: <FAQIcon /> },
-  { id: 10, type: "divider" },
-  { id: 11, type: "title", label: "PROJECTS" },
-  {
-    id: 12,
-    label: "My recent",
-    link: "",
-    icon: <Dot size="small" color="warning" />,
-  },
-  {
-    id: 13,
-    label: "Starred",
-    link: "",
-    icon: <Dot size="small" color="primary" />,
-  },
-  {
-    id: 14,
-    label: "Background",
-    link: "",
-    icon: <Dot size="small" color="secondary" />,
-  },
+	{
+		id: 0,
+		label: "Dashboard",
+		link: "/app/dashboard",
+		icon: <HomeIcon/>
+	},
+	{
+		id: 1,
+		label: "Payloads",
+		link: "/app/typography",
+		icon: <PayloadsIcon/>,
+	},
+	{
+		id: 2,
+		label: "Attacks",
+		link: "/app/tables",
+		icon: <AttacksIcon/>
+		},
+	{
+		id: 3,
+		label: "Infected devices",
+		link: "/app/notifications",
+		icon: <InfectedDevicesIcon/>,
+	},
+	{
+		id: 4,
+		label: "Team",
+		link: "/app/ui",
+		icon: <TeamIcon/>,
+		children: [
+			{label: "Icons", link: "/app/ui/icons"},
+			{label: "Charts", link: "/app/ui/charts"},
+			{label: "Maps", link: "/app/ui/maps"},
+		],
+	},
+	{id: 5, type: "divider"}
 ];
 
-function Sidebar({ location }) {
-  var classes = useStyles();
-  var theme = useTheme();
+function Sidebar({location}) {
+	let classes = useStyles();
+	let theme = useTheme();
 
-  // global
-  var { isSidebarOpened } = useLayoutState();
-  var layoutDispatch = useLayoutDispatch();
+	// global
+	let {isSidebarOpened} = useLayoutState();
+	let layoutDispatch = useLayoutDispatch();
 
-  // local
-  var [isPermanent, setPermanent] = useState(true);
+	// local
+	let [isPermanent, setPermanent] = useState(true);
 
-  useEffect(function() {
-    window.addEventListener("resize", handleWindowWidthChange);
-    handleWindowWidthChange();
-    return function cleanup() {
-      window.removeEventListener("resize", handleWindowWidthChange);
-    };
-  });
+	useEffect(function () {
+		window.addEventListener("resize", handleWindowWidthChange);
+		handleWindowWidthChange();
+		return function cleanup() {
+			window.removeEventListener("resize", handleWindowWidthChange);
+		};
+	});
 
-  return (
-    <Drawer
-      variant={isPermanent ? "permanent" : "temporary"}
-      className={classNames(classes.drawer, {
-        [classes.drawerOpen]: isSidebarOpened,
-        [classes.drawerClose]: !isSidebarOpened,
-      })}
-      classes={{
-        paper: classNames({
-          [classes.drawerOpen]: isSidebarOpened,
-          [classes.drawerClose]: !isSidebarOpened,
-        }),
-      }}
-      open={isSidebarOpened}
-    >
-      <div className={classes.toolbar} />
-      <div className={classes.mobileBackButton}>
-        <IconButton onClick={() => toggleSidebar(layoutDispatch)}>
-          <ArrowBackIcon
-            classes={{
-              root: classNames(classes.headerIcon, classes.headerIconCollapse),
-            }}
-          />
-        </IconButton>
-      </div>
-      <List className={classes.sidebarList}>
-        {structure.map(link => (
-          <SidebarLink
-            key={link.id}
-            location={location}
-            isSidebarOpened={isSidebarOpened}
-            {...link}
-          />
-        ))}
-      </List>
-    </Drawer>
-  );
+	return (
+		<Drawer
+			variant={isPermanent ? "permanent" : "temporary"}
+			className={classNames(classes.drawer, {
+				[classes.drawerOpen]: isSidebarOpened,
+				[classes.drawerClose]: !isSidebarOpened,
+			})}
+			classes={{
+				paper: classNames({
+					[classes.drawerOpen]: isSidebarOpened,
+					[classes.drawerClose]: !isSidebarOpened,
+				}),
+			}}
+			open={isSidebarOpened}
+		>
+			<div className={classes.toolbar}/>
+			<div className={classes.mobileBackButton}>
+				<IconButton onClick={() => toggleSidebar(layoutDispatch)}>
+					<ArrowBackIcon classes={{root: classNames(classes.headerIcon, classes.headerIconCollapse)}}/>
+				</IconButton>
+			</div>
+			<List className={classes.sidebarList}>
+				{structure.map(link => (<SidebarLink key={link.id} location={location} isSidebarOpened={isSidebarOpened}{...link}/>))}
+			</List>
+		</Drawer>
+	);
 
-  // ##################################################################
-  function handleWindowWidthChange() {
-    var windowWidth = window.innerWidth;
-    var breakpointWidth = theme.breakpoints.values.md;
-    var isSmallScreen = windowWidth < breakpointWidth;
+	// ##################################################################
+	function handleWindowWidthChange() {
+		let windowWidth = window.innerWidth;
+		let breakpointWidth = theme.breakpoints.values.md;
+		let isSmallScreen = windowWidth < breakpointWidth;
 
-    if (isSmallScreen && isPermanent) {
-      setPermanent(false);
-    } else if (!isSmallScreen && !isPermanent) {
-      setPermanent(true);
-    }
-  }
+		if (isSmallScreen && isPermanent) {
+			setPermanent(false);
+		} else if (!isSmallScreen && !isPermanent) {
+			setPermanent(true);
+		}
+	}
 }
 
 export default withRouter(Sidebar);
