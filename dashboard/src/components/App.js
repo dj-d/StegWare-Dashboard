2c1eeafb2ddaed1ca0b565d7e71ed7b88ad03af9
Daniele Albanese <daniele.albanese98@gmail.com>
2021-03-04 00:33:20 +0100

    App.js -> reformat code

--- dashboard/src/components/App.js
+++ dashboard/src/components/App.js
@@ -13,17 +13,13 @@ import { useUserState } from "../context/UserContext";
 
 export default function App() {
   // global
-  var { isAuthenticated } = useUserState();
+  let { isAuthenticated } = useUserState();
 
   return (
     <HashRouter>
       <Switch>
         <Route exact path="/" render={() => <Redirect to="/app/dashboard" />} />
-        <Route
-          exact
-          path="/app"
-          render={() => <Redirect to="/app/dashboard" />}
-        />
+        <Route exact path="/app" render={() => <Redirect to="/app/dashboard" />} />
         <PrivateRoute path="/app" component={Layout} />
         <PublicRoute path="/login" component={Login} />
         <Route component={Error} />
