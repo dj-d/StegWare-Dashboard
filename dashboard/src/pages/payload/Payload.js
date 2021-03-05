86db0af251be8c29c6fa86789f8d1ba10bfd89e2
Daniele Albanese <daniele.albanese98@gmail.com>
2021-03-05 02:00:38 +0100

    Page Payload -> Added request api (To review)

--- dashboard/src/pages/payload/Payload.js
+++ dashboard/src/pages/payload/Payload.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, {useEffect, useState} from "react";
 import {Grid} from "@material-ui/core";
 
 // styles
@@ -11,11 +11,43 @@ import Widget from "../../components/Widget";
 function PayloadPage() {
 	let classes = useStyles();
 
-	return (
-		<>
-			<PageTitle title="Payloads" />
-		</>
-	)
+	const [error, setError] = useState(null);
+	const [isLoaded, setIsLoaded] = useState(false);
+	const [items, setItems] = useState([]);
+
+	useEffect(() => {
+		fetch("http://192.168.0.246:9999/api/payload")
+			.then(res => res.json())
+			.then(
+				(result) => {
+					setIsLoaded(true);
+					setItems(result);
+				},
+				// Note: it's important to handle errors here
+				// instead of a catch() block so that we don't swallow
+				// exceptions from actual bugs in components.
+				(error) => {
+					setIsLoaded(true);
+					setError(error);
+				}
+			)
+	}, [])
+
+	if (error) {
+		return <div>Error: {error.message}</div>;
+	} else if (!isLoaded) {
+		return <div>Loading...</div>;
+	} else {
+		return (
+			<ul>
+				{items.map(item => (
+					<li key={item._id}>
+						{item.name}
+					</li>
+				))}
+			</ul>
+		);
+	}
 }
 
 export default PayloadPage;
