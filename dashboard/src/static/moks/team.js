398046bf1abf3c4ccd2ae0b9b623fa9dbeb7a298
Daniele Albanese <daniele.albanese98@gmail.com>
2021-03-26 00:04:34 +0100

    Added mock of team

---
+++ dashboard/src/static/moks/team.js
@@ -0,0 +1,20 @@
+import developer from "../images/team/Developer.jpg";
+import supervisor from "../images/team/Supervisor.jpg";
+import coSupervisor from "../images/team/Co-Supervisor.jpg";
+
+const team = [
+	{
+		name: "Ing. Francesco Mercaldo",
+		img: supervisor
+	},
+	{
+		name: "Dott.ssa Rosangela Casolare",
+		img: coSupervisor
+	},
+	{
+		name: "Daniele Albanese",
+		img: developer
+	}
+]
+
+export default team
