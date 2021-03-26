d8a24a33210625f84868cda8f800a942055781b4
Daniele Albanese <daniele.albanese98@gmail.com>
2021-03-26 17:17:05 +0100

    CardComponent -> Optimized icons import

--- dashboard/src/components/Card/CardComponent.js
+++ dashboard/src/components/Card/CardComponent.js
@@ -5,9 +5,11 @@ import {Card, CardContent, CardMedia, IconButton, Link, Typography} from "@mater
 import useStyles from "./styles";
 
 // icons
-import GitHubIcon from "@material-ui/icons/GitHub";
-import LinkedInIcon from "@material-ui/icons/LinkedIn";
-import FacebookIcon from "@material-ui/icons/Facebook";
+import {
+	GitHub as GitHubIcon,
+	LinkedIn as LinkedInIcon,
+	Facebook as FacebookIcon
+} from "@material-ui/icons";
 
 export default function CardComponent({user}) {
 	let classes = useStyles();
