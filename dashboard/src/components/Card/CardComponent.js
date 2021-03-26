2e90a8981d9d3dbb5bcda4093c9c1712e20a5c0a
Daniele Albanese <daniele.albanese98@gmail.com>
2021-03-26 09:47:05 +0100

    Added CardComponent + Updated Team page

---
+++ dashboard/src/components/Card/CardComponent.js
@@ -0,0 +1,56 @@
+import React from "react";
+import {Card, CardContent, CardMedia, IconButton, Link, Typography} from "@material-ui/core";
+
+// styles
+import useStyles from "./styles";
+
+// icons
+import GitHubIcon from "@material-ui/icons/GitHub";
+import LinkedInIcon from "@material-ui/icons/LinkedIn";
+import FacebookIcon from "@material-ui/icons/Facebook";
+
+export default function CardComponent({user}) {
+	let classes = useStyles();
+
+	return (
+		<>
+			<Card className={classes.card}>
+				<CardMedia image={user.image} title={user.name} className={classes.media}/>
+
+				<CardContent className={classes.content}>
+					<div className={classes.cardText}>
+						<Typography align="center" variant="h3" className={classes.title}>
+							{user.name}
+						</Typography>
+					</div>
+
+					<div align="center" className={classes.cardLink}>
+						{user.social.github.length > 0 && (
+							<Link href={user.social.github} target={'_blank'}>
+								<IconButton aria-label={"github"}>
+									<GitHubIcon color="primary" className={classes.icon}/>
+								</IconButton>
+							</Link>
+						)}
+
+						{user.social.linkedin.length > 0 && (
+							<Link href={user.social.linkedin} target={'_blank'}>
+								<IconButton aria-label={"github"}>
+									<LinkedInIcon color="primary" className={classes.icon}/>
+								</IconButton>
+							</Link>
+						)}
+
+						{user.social.facebook.length > 0 && (
+							<Link href={user.social.facebook} target={'_blank'}>
+								<IconButton aria-label={"github"}>
+									<FacebookIcon color="primary" className={classes.icon}/>
+								</IconButton>
+							</Link>
+						)}
+					</div>
+				</CardContent>
+			</Card>
+		</>
+	);
+}
--- dashboard/src/pages/team/Team.js
+++ dashboard/src/pages/team/Team.js
@@ -1,22 +1,13 @@
 import React from "react";
-import {Card, CardContent, CardMedia, Grid, IconButton, Link, Typography} from "@material-ui/core";
+import {Grid} from "@material-ui/core";
 
 // component
 import PageTitle from "../../components/PageTitle";
-
-// icons
-import GitHubIcon from '@material-ui/icons/GitHub';
-import LinkedInIcon from '@material-ui/icons/LinkedIn';
-import FacebookIcon from '@material-ui/icons/Facebook';
-
-// styles
-import useStyles from "./styles";
+import CardComponent from "../../components/Card/CardComponent";
 
 import team_data from "../../static/mocks/team";
 
 function TeamPage() {
-	let classes = useStyles();
-
 	return (
 		<>
 			<PageTitle title="Team"/>
@@ -24,43 +15,7 @@ function TeamPage() {
 			<Grid container={true} spacing={4} justify="center">
 				{team_data.map(user => (
 					<Grid item lg={3} md={4} sm={6} xs={12}>
-						<Card className={classes.card}>
-							<CardMedia image={user.img} title={user.name} className={classes.media}/>
-
-							<CardContent className={classes.content}>
-								<div className={classes.cardText}>
-									<Typography align="center" className={classes.title}>
-										{user.name}
-									</Typography>
-								</div>
-
-								<div align="center" className={classes.cardLink}>
-									{user.github.length > 0 && (
-										<Link href={user.github} target={'_blank'}>
-											<IconButton aria-label={"github"}>
-												<GitHubIcon color="primary" className={classes.icon}/>
-											</IconButton>
-										</Link>
-									)}
-
-									{user.linkedin.length > 0 && (
-										<Link href={user.linkedin} target={'_blank'}>
-											<IconButton aria-label={"github"}>
-												<LinkedInIcon color="primary" className={classes.icon}/>
-											</IconButton>
-										</Link>
-									)}
-
-									{user.facebook.length > 0 && (
-										<Link href={user.facebook} target={'_blank'}>
-											<IconButton aria-label={"github"}>
-												<FacebookIcon color="primary" className={classes.icon}/>
-											</IconButton>
-										</Link>
-									)}
-								</div>
-							</CardContent>
-						</Card>
+						<CardComponent user={user} />
 					</Grid>
 				))}
 			</Grid>
