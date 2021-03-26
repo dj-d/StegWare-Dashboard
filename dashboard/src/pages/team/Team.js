d6deb622d63078ac38b0c258c3af4e27046058ce
Daniele Albanese <daniele.albanese98@gmail.com>
2021-03-26 02:41:01 +0100

    Completed Team page

--- dashboard/src/pages/team/Team.js
+++ dashboard/src/pages/team/Team.js
@@ -1,14 +1,18 @@
 import React from "react";
-import {Avatar, CardActionArea, CardContent, Grid} from "@material-ui/core";
+import {Card, CardContent, CardMedia, Grid, IconButton, Link, Typography} from "@material-ui/core";
+
+// component
+import PageTitle from "../../components/PageTitle";
+
+// icons
+import GitHubIcon from '@material-ui/icons/GitHub';
+import LinkedInIcon from '@material-ui/icons/LinkedIn';
+import FacebookIcon from '@material-ui/icons/Facebook';
 
 // styles
 import useStyles from "./styles";
 
-// component
-import PageTitle from "../../components/PageTitle";
-import {Typography} from "../../components/Wrappers";
-
-import team_data from "../../static/moks/team"
+import team_data from "../../static/mocks/team";
 
 function TeamPage() {
 	let classes = useStyles();
@@ -17,17 +21,46 @@ function TeamPage() {
 		<>
 			<PageTitle title="Team"/>
 
-			<Grid container={true} spacing={4} justify="center" alignItems="center">
+			<Grid container={true} spacing={4} justify="center">
 				{team_data.map(user => (
-					<Grid item lg={3} md={4} sm={6} xs={12} justify="center">
-						<CardActionArea>
-							<CardContent>
-								<Avatar alt={user.name} src={user.img} className={classes.avatar}/>
-								<Typography variant="h3" align="center">
-									{user.name}
-								</Typography>
+					<Grid item lg={3} md={4} sm={6} xs={12}>
+						<Card className={classes.card}>
+							<CardMedia image={user.img} title={user.name} className={classes.media}/>
+
+							<CardContent className={classes.content}>
+								<div className={classes.cardText}>
+									<Typography align="center" className={classes.title}>
+										{user.name}
+									</Typography>
+								</div>
+
+								<div align="center" className={classes.cardLink}>
+									{user.github.length > 0 && (
+										<Link href={user.github} target={'_blank'}>
+											<IconButton aria-label={"github"}>
+												<GitHubIcon color="primary" className={classes.icon}/>
+											</IconButton>
+										</Link>
+									)}
+
+									{user.linkedin.length > 0 && (
+										<Link href={user.linkedin} target={'_blank'}>
+											<IconButton aria-label={"github"}>
+												<LinkedInIcon color="primary" className={classes.icon}/>
+											</IconButton>
+										</Link>
+									)}
+
+									{user.facebook.length > 0 && (
+										<Link href={user.facebook} target={'_blank'}>
+											<IconButton aria-label={"github"}>
+												<FacebookIcon color="primary" className={classes.icon}/>
+											</IconButton>
+										</Link>
+									)}
+								</div>
 							</CardContent>
-						</CardActionArea>
+						</Card>
 					</Grid>
 				))}
 			</Grid>
--- dashboard/src/pages/team/styles.js
+++ dashboard/src/pages/team/styles.js
@@ -1,8 +1,32 @@
 import {makeStyles} from "@material-ui/styles";
 
 export default makeStyles({
-	avatar: {
-		height: "75%",
-		width: "75%"
+	card: {
+		display: "block",
+		margin: 5,
+		borderRadius: 10,
+	},
+	media: {
+		width: "auto",
+		height: 300,
+		flexBasis: "50%"
+	},
+	content: {
+		display: "flex",
+		flexDirection: "column",
+		flexBasis: "50%",
+		backgroundColor: "#ffffff",
+	},
+	cardText: {
+		flex: "1 0 auto",
+		marginBottom: 15,
+	},
+	title: {
+		fontWeight: "bold",
+	},
+	cardLink: {
+		align: "center"
+	},
+	icon: {
 	}
 });
