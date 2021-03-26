e47fb466ffc54655b323091fda4f1d3154c441d9
Daniele Albanese <daniele.albanese98@gmail.com>
2021-03-26 02:38:09 +0100

    Updated team mocks

--- dashboard/src/static/moks/team.js
+++ dashboard/src/static/moks/team.js
@@ -5,15 +5,24 @@ import coSupervisor from "../images/team/Co-Supervisor.jpg";
 const team = [
 	{
 		name: "Ing. Francesco Mercaldo",
-		img: supervisor
+		img: supervisor,
+		github: "https://github.com/FrancescoMercaldo",
+		linkedin: "https://www.linkedin.com/in/francesco-mercaldo-77239113/",
+		facebook: "https://www.facebook.com/mercaldofrancesco",
 	},
 	{
 		name: "Dott.ssa Rosangela Casolare",
-		img: coSupervisor
+		img: coSupervisor,
+		github: "",
+		linkedin: "",
+		facebook: "https://www.facebook.com/rosangela.casolare",
 	},
 	{
 		name: "Daniele Albanese",
-		img: developer
+		img: developer,
+		github: "http://github.com/dj-d",
+		linkedin: "https://www.linkedin.com/in/daniele-albanese-1aab1091/",
+		facebook: "https://www.facebook.com/daniele.albanese.581",
 	}
 ]
 
