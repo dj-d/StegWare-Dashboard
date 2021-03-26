d25d5518695cd5f23e99b20bff0d026d315b88a1
Daniele Albanese <daniele.albanese98@gmail.com>
2021-03-26 09:46:21 +0100

    Team mock -> Updated format

--- dashboard/src/static/mocks/team.js
+++ dashboard/src/static/mocks/team.js
@@ -5,24 +5,30 @@ import coSupervisor from "../images/team/Co-Supervisor.jpg";
 const team = [
 	{
 		name: "Ing. Francesco Mercaldo",
-		img: supervisor,
-		github: "https://github.com/FrancescoMercaldo",
-		linkedin: "https://www.linkedin.com/in/francesco-mercaldo-77239113/",
-		facebook: "https://www.facebook.com/mercaldofrancesco",
+		image: supervisor,
+		social: {
+			github: "https://github.com/FrancescoMercaldo",
+			linkedin: "https://www.linkedin.com/in/francesco-mercaldo-77239113/",
+			facebook: "https://www.facebook.com/mercaldofrancesco",
+		}
 	},
 	{
 		name: "Dott.ssa Rosangela Casolare",
-		img: coSupervisor,
-		github: "",
-		linkedin: "",
-		facebook: "https://www.facebook.com/rosangela.casolare",
+		image: coSupervisor,
+		social: {
+			github: "",
+			linkedin: "",
+			facebook: "https://www.facebook.com/rosangela.casolare",
+		}
 	},
 	{
 		name: "Daniele Albanese",
-		img: developer,
-		github: "http://github.com/dj-d",
-		linkedin: "https://www.linkedin.com/in/daniele-albanese-1aab1091/",
-		facebook: "https://www.facebook.com/daniele.albanese.581",
+		image: developer,
+		social: {
+			github: "http://github.com/dj-d",
+			linkedin: "https://www.linkedin.com/in/daniele-albanese-1aab1091/",
+			facebook: "https://www.facebook.com/daniele.albanese.581",
+		}
 	}
 ]
 
