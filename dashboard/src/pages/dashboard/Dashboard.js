64bd9e1166d21aa4005bdc8a5b0b24018fc397f2
dj-d <nicodani>
2021-05-01 01:29:54 +0200

    pages/dashboard -> Added Logo + Fixed styles + Deleted file mock

--- dashboard/src/pages/dashboard/Dashboard.js
+++ dashboard/src/pages/dashboard/Dashboard.js
@@ -1,32 +1,34 @@
 import React from "react";
-import {Grid, Button} from "@material-ui/core";
+import { Grid, Card, CardMedia, CardActionArea } from "@material-ui/core";
+
+// components
+import PageTitle from "../../components/PageTitle";
 
 // styles
 import useStyles from "./styles";
 
-// components
-import mock from "./mock";
-import Widget from "../../components/Widget";
-import PageTitle from "../../components/PageTitle";
-import Table from "./components/Table/Table";
+// images
+import logo from "../../static/images/StegWare_logo.png"
 
 export default function Dashboard() {
-	let classes = useStyles();
+    let classes = useStyles();
 
-	return (
-		<>
-			<PageTitle title="Dashboard" button={
-				<Button variant="contained" size="medium" color="secondary">
-					Refresh
-				</Button>}
-			/>
-			<Grid container spacing={4}>
-				<Grid item xs={12}>
-					<Widget title="Support Requests" upperTitle noBodyPadding bodyClass={classes.tableWidget}>
-						<Table data={mock.table}/>
-					</Widget>
-				</Grid>
-			</Grid>
-		</>
-	);
+    return (
+        <>
+            <PageTitle title="Dashboard"/>
+
+            <Grid container spacing={4}>
+                <Grid item lg={3} md={4} sm={6} xs={12}>
+                    <Card>
+                        <CardActionArea>
+                            <CardMedia
+                                component="img"
+                                image={logo}
+                            />
+                        </CardActionArea>
+                    </Card>
+                </Grid>
+            </Grid>
+        </>
+    );
 }
