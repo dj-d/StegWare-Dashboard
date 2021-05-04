85086d0431270258807c1a4575fe47d6dec2ed2d
dj-d <nicodani>
2021-05-04 12:28:17 +0200

    context/LayoutContext -> Fixed code smell + refactoring + reformatting

--- dashboard/src/context/LayoutContext.js
+++ dashboard/src/context/LayoutContext.js
@@ -1,52 +1,56 @@
-import React from "react";
+import React, { useReducer, createContext, useContext } from "react";
 
-var LayoutStateContext = React.createContext();
-var LayoutDispatchContext = React.createContext();
+const TOGGLE_SIDEBAR = "TOGGLE_SIDEBAR";
+
+let LayoutStateContext = createContext();
+let LayoutDispatchContext = createContext();
 
 function layoutReducer(state, action) {
-  switch (action.type) {
-    case "TOGGLE_SIDEBAR":
-      return { ...state, isSidebarOpened: !state.isSidebarOpened };
-    default: {
-      throw new Error(`Unhandled action type: ${action.type}`);
+    if (action.type === TOGGLE_SIDEBAR) {
+        return { ...state, isSidebarOpened: !state.isSidebarOpened };
+    } else {
+        throw new Error(`Unhandled action type: ${action.type}`);
     }
-  }
 }
 
 function LayoutProvider({ children }) {
-  var [state, dispatch] = React.useReducer(layoutReducer, {
-    isSidebarOpened: true,
-  });
-  return (
-    <LayoutStateContext.Provider value={state}>
-      <LayoutDispatchContext.Provider value={dispatch}>
-        {children}
-      </LayoutDispatchContext.Provider>
-    </LayoutStateContext.Provider>
-  );
+    const [state, dispatch] = useReducer(layoutReducer, {
+        isSidebarOpened: true,
+    });
+
+    return (
+        <LayoutStateContext.Provider value={state}>
+            <LayoutDispatchContext.Provider value={dispatch}>
+                {children}
+            </LayoutDispatchContext.Provider>
+        </LayoutStateContext.Provider>
+    );
 }
 
 function useLayoutState() {
-  var context = React.useContext(LayoutStateContext);
-  if (context === undefined) {
-    throw new Error("useLayoutState must be used within a LayoutProvider");
-  }
-  return context;
+    let context = useContext(LayoutStateContext);
+
+    if (context === undefined) {
+        throw new Error("useLayoutState must be used within a LayoutProvider");
+    }
+
+    return context;
 }
 
 function useLayoutDispatch() {
-  var context = React.useContext(LayoutDispatchContext);
-  if (context === undefined) {
-    throw new Error("useLayoutDispatch must be used within a LayoutProvider");
-  }
-  return context;
+    let context = useContext(LayoutDispatchContext);
+
+    if (context === undefined) {
+        throw new Error("useLayoutDispatch must be used within a LayoutProvider");
+    }
+
+    return context;
+}
+
+function toggleSidebar(dispatch) {
+    dispatch({
+        type: TOGGLE_SIDEBAR
+    });
 }
 
 export { LayoutProvider, useLayoutState, useLayoutDispatch, toggleSidebar };
-
-// ###########################################################
-function toggleSidebar(dispatch) {
-  dispatch({
-    type: "TOGGLE_SIDEBAR",
-  });
-}
