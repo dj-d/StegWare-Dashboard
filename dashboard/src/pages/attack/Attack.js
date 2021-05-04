d6e8519c8d35ae1a38f744edcc7816ca95cac4f6
dj-d <nicodani>
2021-05-04 12:31:38 +0200

    pages/Attack -> Fixed code smell + reformatting code

--- dashboard/src/pages/attack/Attack.js
+++ dashboard/src/pages/attack/Attack.js
@@ -1,21 +1,12 @@
 import React from "react";
-import {Grid} from "@material-ui/core";
-
-// styles
-import useStyles from "./styles";
 
 // component
 import PageTitle from "../../components/PageTitle";
-import Widget from "../../components/Widget";
 
-function AttackPage() {
-	let classes = useStyles();
-
-	return (
-		<>
-			<PageTitle title="Attacks" />
-		</>
-	)
+export default function Attack() {
+    return (
+        <>
+            <PageTitle title="Attacks"/>
+        </>
+    )
 }
-
-export default AttackPage;
