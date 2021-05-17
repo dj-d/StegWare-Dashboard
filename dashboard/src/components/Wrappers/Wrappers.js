ea6bcc061b9deba447ce2a991fb78adc422d3373
dj-d <daniele.albanese98@gmail.com>
2021-05-17 16:11:30 +0200

    components/Wrappers -> Updated

--- dashboard/src/components/Wrappers/Wrappers.js
+++ dashboard/src/components/Wrappers/Wrappers.js
@@ -2,10 +2,4 @@ import Button from "./components/Button/Button";
 import Card from "./components/Card/Card";
 import Input from "./components/Input/Input";
 
-const Wrappers = {
-    Button,
-    Card,
-    Input
-}
-
-export default Wrappers;
+export { Button, Card, Input };
