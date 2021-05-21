03a71641759f2124d53e32d06f45029d17255e5b
dj-d <daniele.albanese98@gmail.com>
2021-05-21 16:32:00 +0200

    components/Wrappers -> Added Typography wrapped component

--- dashboard/src/components/Wrappers/Wrappers.js
+++ dashboard/src/components/Wrappers/Wrappers.js
@@ -1,5 +1,6 @@
 import Button from "./components/Button/Button";
 import Card from "./components/Card/Card";
 import Input from "./components/Input/Input";
+import Typography from "./components/Typography/Typography";
 
-export { Button, Card, Input };
+export { Button, Card, Input, Typography };
