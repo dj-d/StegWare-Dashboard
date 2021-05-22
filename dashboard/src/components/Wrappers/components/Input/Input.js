9bc004855e1be704c5c64e31fcc7479e1bf5a56c
dj-d <daniele.albanese98@gmail.com>
2021-05-22 19:55:13 +0200

    components/Wrappers/component/Input -> Added parameters: id, variant and disabled

--- dashboard/src/components/Wrappers/components/Input/Input.js
+++ dashboard/src/components/Wrappers/components/Input/Input.js
@@ -1,15 +1,16 @@
 import React from "react";
 import { TextField } from "@material-ui/core";
 
-export default function Input({ name, label, value, disable, defaultValue, error = null, onChange }) {
+export default function Input({ id, name, label, value, disabled, defaultValue, variant, error = null, onChange }) {
     return (
         <TextField
-            variant="outlined"
+            id={id}
             label={label}
             name={name}
             value={value}
-            disable={disable}
+            disabled={disabled}
             defaultValue={defaultValue}
+            variant={variant || "outlined"}
             onChange={onChange}
             {...(error && { error: true, helperText: error })}
         />
