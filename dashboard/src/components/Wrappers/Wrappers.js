0081cf584b7f3cddf4aa845dabbffe943a545385
dj-d <daniele.albanese98@gmail.com>
2021-05-22 19:54:06 +0200

    components/Wrappers -> Added export of Select component

--- dashboard/src/components/Wrappers/Wrappers.js
+++ dashboard/src/components/Wrappers/Wrappers.js
@@ -1,6 +1,7 @@
 import Button from "./components/Button/Button";
 import Card from "./components/Card/Card";
 import Input from "./components/Input/Input";
+import Select from "./components/Select/Select";
 import Typography from "./components/Typography/Typography";
 
-export { Button, Card, Input, Typography };
+export { Button, Card, Input, Select, Typography };
