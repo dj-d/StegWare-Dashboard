fbbbe4d6b3798ad360578b2980e29190327a68fb
dj-d <daniele.albanese98@gmail.com>
2021-05-23 00:21:32 +0200

    components/PayloadAction/Detail -> Removed unused styles options

--- dashboard/src/components/PayloadAction/Detail/styles.js
+++ dashboard/src/components/PayloadAction/Detail/styles.js
@@ -11,36 +11,7 @@ export default makeStyles(theme => ({
     textFiled: {
         color: "#FFFFFF"
     },
-    card: {
-        display: "block",
-        margin: 5,
-        borderRadius: 10,
-        boxShadow: theme.customShadows.widget,
-        minHeight: "100%"
-    },
-    cardContent: {
-        margin: 5,
-        marginLeft: theme.spacing(2)
-    },
     button: {
         boxShadow: "0px 3px 3px -2px #000000"
-    },
-    header: {
-        margin: theme.spacing(2)
-    },
-    headerTitle: {
-        overflow: "hidden",
-        whiteSpace: "nowrap",
-        maxWidth: "12em",
-        textOverflow: "ellipsis",
-        textTransform: "capitalize",
-        color: theme.palette.text.primary
-    },
-    headerSubtitle: {
-        overflow: "hidden",
-        whiteSpace: "nowrap",
-        maxWidth: "15em",
-        textOverflow: "ellipsis",
-        color: theme.palette.text.secondary
     }
 }))
