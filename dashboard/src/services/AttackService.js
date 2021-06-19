d536219b29ebcdce33728ef2695e4e4587840e1b
dj-d <daniele.albanese98@gmail.com>
2021-06-20 01:21:48 +0200

    service/AttackService -> Updated endpoints

--- dashboard/src/services/AttackService.js
+++ dashboard/src/services/AttackService.js
@@ -2,14 +2,14 @@ import Api from './Api';
 
 export default {
 	fetchAttacks() {
-		return Api().get('attacks')
+		return Api().get('attack')
 	},
 
 	fetchAttack(attack_id) {
-		return Api().get('attacks?attack_id=' + attack_id)
+		return Api().get('attack?attack_id=' + attack_id)
 	},
 
 	deleteAttack(attack_id) {
-		return Api().delete('attacks?attack_id=' + attack_id)
+		return Api().delete('attack?attack_id=' + attack_id)
 	}
 }
