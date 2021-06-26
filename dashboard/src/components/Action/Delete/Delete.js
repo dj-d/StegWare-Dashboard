506fee5e63e11430e715346db401f81dc5254029
dj-d <daniele.albanese98@gmail.com>
2021-06-26 10:02:32 +0200

    components/Action/Delete -> Moved into dedicated folder + Transformed from a specific to a general component

--- dashboard/src/components/Action/Delete/Delete.js
+++ dashboard/src/components/Action/Delete/Delete.js
@@ -4,10 +4,11 @@ import { Paper, Typography } from "@material-ui/core";
 import { Button } from "../../Wrappers/Wrappers";
 
 // service
-import PayloadService from "../../../services/PayloadService";
+import PayloadService from "../../../services/PayloadService"
 
 // styles
 import useStyles from "./styles";
+import AttackService from "../../../services/AttackService";
 
 export default function Delete({ ...props }) {
     const classes = useStyles();
@@ -18,13 +19,24 @@ export default function Delete({ ...props }) {
 
     // TODO: To complete
     function deleteAction() {
-        PayloadService.deletePayload(props.payloadID)
-            .then()
-            .catch()
-            .finally(() => {
-                // Refresh page
-                window.location.reload();
-        })
+        if (props.collection === "payload") {
+            PayloadService.deletePayload(props.payloadId)
+                .then()
+                .catch()
+                .finally(() => {
+                    // Refresh page
+                    window.location.reload();
+                })
+        } else if (props.collection === "attack") {
+            AttackService.deleteAttack(props.attackId)
+                .then()
+                .catch()
+                .finally(() => {
+                    window.location.reload();
+                })
+        } else {
+            console.log("Delete action - Not valid collection");
+        }
     }
 
     return (
