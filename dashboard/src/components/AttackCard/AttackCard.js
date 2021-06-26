5d3d0bd1d8978a82f24ca914aa224eab10a11c80
dj-d <daniele.albanese98@gmail.com>
2021-06-26 10:04:30 +0200

    components/AttackCard -> Added Delete button

--- dashboard/src/components/AttackCard/AttackCard.js
+++ dashboard/src/components/AttackCard/AttackCard.js
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from "react";
 import { Button, Card, Typography } from "../Wrappers/Wrappers";
-import { List, ListItem, ListItemIcon, ListItemText } from "@material-ui/core";
+import { List, ListItem, ListItemIcon, ListItemText, Popper } from "@material-ui/core";
 
 // icons
 import {
@@ -9,9 +9,30 @@ import {
     Delete as DeleteIcon
 } from "@material-ui/icons";
 import PayloadService from "../../services/PayloadService";
+import Delete from "../Action/Delete/Delete";
 
 export default function AttackCard({ attack }) {
     const [payloadName, setPayloadName] = useState(null)
+
+    const [anchorEl, setAnchorEl] = useState(null);
+    const [placement, setPlacement] = useState();
+    const [openDelete, setOpenDelete] = useState(false);
+
+    const [openResult, setOpenResult] = useState(false);
+
+    const handleClickResult = () => {
+        setOpenResult(!openResult);
+    }
+
+    const handleClick = (newPlacement) => (event) => {
+        setAnchorEl(event.currentTarget);
+        setOpenDelete((prev) => placement !== newPlacement || !prev);
+        setPlacement(newPlacement);
+    }
+
+    function changeDeleteVisibility(newValue) {
+        setOpenDelete(newValue);
+    }
 
     const data = [
         "Parse time: " + attack.timing.parseTime,
@@ -32,6 +53,10 @@ export default function AttackCard({ attack }) {
 
     return (
         <>
+            <Popper open={openDelete} anchorEl={anchorEl} placement={placement} disablePortal={true}>
+                <Delete changeVisibility={changeDeleteVisibility} attackId={attack._id} collection="attack"/>
+            </Popper>
+
             <Card
                 cardHeader={true}
                 headerTitle={attack.device.model}
@@ -71,6 +96,7 @@ export default function AttackCard({ attack }) {
                         </Button>
                         <Button
                             color="secondary"
+                            onClick={handleClick('bottom')}
                             startIcon={<DeleteIcon/>}
                         >
                             Delete
